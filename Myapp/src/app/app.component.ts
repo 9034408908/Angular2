a97455c912f2cca613b9049e5531a6e988bc26df
unknown <kumarudit.24@gmail.com>
2018-05-20 15:59:18 +0530

    added file for xyz

--- Myapp/src/app/app.component.ts
+++ Myapp/src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  private a:string="10";
+  private b:string="20";
+  private c: number;
+  add(){
+    alert("hii")
+    this.c=parseInt(this.a)+parseInt(this.b);
+  }
+  private p : string="60";
+  private ch : string="50";
+  private h : string="70";
+  private m : string="60";
+  private e : string="80";
+  private av : number;
+  avg(){
+    alert("Averge")
+    this.av = (parseFloat(this.p) + parseFloat(this.ch) + parseFloat(this.h) + parseFloat(this.m) + parseFloat(this.e))/5;
+  }
+
 }
